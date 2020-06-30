aba3b27fad8b00325956894b54eb06efb3ad907f
carlos Narvaez <carlos_rothn@yahoo.es>
2020-06-30 15:01:35 -0500

    test

---
+++ js/gracias.js
@@ -0,0 +1,28 @@
+﻿
+var myRj;
+
+$(document).ready(function () {
+
+    localStorage.setItem("DATA_CARGADA", 0);
+    localStorage.setItem("POS_REGISTRO", 0);
+    localStorage.setItem("TOTAL_REGISTRO", 0);
+
+    var ListaIdPreguntas = [-1, -1, -1, -1];
+    var ListaDecripsPreguntas = ['', '', '', -''];
+    var ListaComentarios = ['', '', '', -''];
+
+    localStorage.setItem("LISTA_IDPREGUNTAS", JSON.stringify(ListaIdPreguntas));
+    localStorage.setItem("LISTA_DESCRIP_PREGUNTAS", JSON.stringify(ListaDecripsPreguntas));
+    localStorage.setItem("LISTA_COMENTARIO_PREGUNTAS", JSON.stringify(ListaComentarios));
+
+    //llamar reloj
+    var Tiempo_ = 5000;
+    myRj = setTimeout(finaliza, Tiempo_);
+
+});
+
+function finaliza() {
+    clearTimeout(myRj);
+    //Cerrar pagina
+    window.close;
+}
